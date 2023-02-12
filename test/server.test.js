255f856561dabc68bf7c81f2ca5165c0b926d6e9
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-02-12 18:07:00 -0300

    base sem typescript

---
+++ test/server.test.js
@@ -0,0 +1,12 @@
+const app = require('../src/app');
+const supertest = require('supertest');
+const request = supertest(app)
+
+test('A aplicação deve responder na porta 3000', () => {
+    return request.get('/').then(res => {
+        let status = res.statusCode;
+        expect(status).toEqual(200);
+    }).catch(e => {
+        console.log(e);
+    })
+})
