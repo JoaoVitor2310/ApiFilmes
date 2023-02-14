5eb8f0b977ec4913261bf0a5c8dc9c99e8449e48
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-02-13 22:13:04 -0300

    mais da base

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+console.log('Deu bom!')
+
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+
+// app.get('/', (req,res) => {
+//     res.send('Oi');
+// })
+
+// module.exports = app;
