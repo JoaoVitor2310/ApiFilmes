24c95563c400e1c95d3facbaf0f76d217eb82223
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-02-26 11:39:10 -0300

    validações de login criadas

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -1,7 +1,33 @@
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 
 const userCreateValidation = () => {
-    return [body('name').isString().withMessage('O nome é obrigatório.')]
+    return [
+        body('name')
+            .isString()
+            .withMessage('O nome é obrigatório.')
+            .isLength({ min: 2 })
+            .withMessage('O nome precisa ter no mínimo 2 caracteres.'),
+        body('email')
+            .isString()
+            .withMessage('O email é obrigatório.')
+            .isEmail()
+            .withMessage('Insira um e-mail válido.'),
+        body('password')
+            .isString()
+            .withMessage('A senha é obrigatória')
+            .isLength({ min: 7 })
+            .withMessage('A senha precisa ter no mínimo 7 caracteres.'),
+        body('confirmPassword')
+            .isString()
+            .withMessage('A confirmação de senha é obrigatória')
+            .custom((value, {req}) => {
+                if(value != req.body.password){
+                    throw new Error('As senhas não são iguais.')
+                }
+                return true
+            })
+            ,
+    ]
 }
 
 module.exports = {
