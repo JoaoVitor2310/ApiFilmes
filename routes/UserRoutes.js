b07395acf2916c9fce6fe5b3b46b12f475052ac5
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-02-26 11:20:56 -0300

    início da criação de middlewares

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//Controllers
 const {register} = require('../controller/UserController');
 
-router.post('/register', register);
+//Middlewares
+const validate = require('../middlewares/handleValidation');
+const {userCreateValidation} = require('../middlewares/userValidations');
+
+//Routes
+router.post('/register', userCreateValidation(), validate, register);
 
 module.exports = router;
