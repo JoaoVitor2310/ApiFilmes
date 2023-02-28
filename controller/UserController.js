8b2f776bc0c6a62400ad110f3167e23316eafa09
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-02-27 21:03:52 -0300

    início do registro, hoje não tive muito tempo por causa da faculdade e prova de mineração de dados amanha

--- controller/UserController.js
+++ controller/UserController.js
@@ -14,6 +14,13 @@ const generateToken = id => {
 const register = async(req, res) => {
     // let time = Date.now();
     // let email = `${time}@gmail.com`;
+    const {name, email, password} = req.body;
+    const user = User.findOne({email});
+    if(user){
+        res.status(422).json({errors: ['Por favor, utilize outro email.']});
+        return;
+    }
+
     email = 'joaovitormatosgouveia@gmail.com'
     res.json({email});
 }
