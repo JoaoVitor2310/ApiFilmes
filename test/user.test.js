ead98a68b415c76c596f96ab36985aa308349394
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-03-05 18:48:36 -0300

    iremos utilizar o before/after all para criar/deletar no banco de dados e não poluir

--- test/user.test.js
+++ test/user.test.js
@@ -2,13 +2,18 @@ const app = require('../src/app');
 const supertest = require('supertest');
 const request = supertest(app);
 
+const userForTests = {name: 'João Vitor', email: 'joaovitormatosgou@gmail.com', password: '1234567', confirmPassword: '1234567'}
+
+// beforeAll(() => {
+//     console.log('Jay11');
+// })
+
+
 describe('Cadastro de usuário', () => {
     test('Deve cadastrar um usuário novo com sucesso', () => {
-        // let time = Date.now();
-        // let email = `${time}@gmail.com`;
-        // email = 'joaovitormatosgouveia@gmail.com';
+
         // topMovies: ['interstellar', 'top gun: maverick', 'homem aranha 2'], watchList: ['avengers', 'joker', 'la la land'
-        let user = {name: 'João Vitor', email: 'joaovitormatosgou@gmail.com', password: '1234567', confirmPassword: '1234567'};
+        let user = { name: 'João Vitor', email: 'joaovitormatosgou@gmail.com', password: '1234567', confirmPassword: '1234567' };
 
         return request.post('/api/users/register')
             .send(user)
@@ -19,18 +24,27 @@ describe('Cadastro de usuário', () => {
                 expect(res.body._id).toBeDefined();
             })
     })
-    // test('Deve impedir que o usuário cadastre um email repetido.', () => {
-    //     let user = {name: 'João Vitor', email: 'joaovitormatosgouveia@gmail.com', password: '1234567', confirmPassword: '1234567'};
 
-    //     return request.post('/api/users/register')
-    //         .send(user)
-    //         .then(res => {
-    //             console.log(res.body.errors);
-    //             expect(res.statusCode).toEqual(201);
-    //             // expect(res.body._id).toEqual(user.email);
-                
-    //         })
-    // })
+    test('Deve impedir que o usuário cadastre um email repetido.', () => {
+        let user = { name: 'João Vitor', email: 'joaovitormatosgouveia@gmail.com', password: '1234567', confirmPassword: '1234567' };
+
+        return request.post('/api/users/register')
+            .send(user)
+            .then(res => {
+                // console.log(res.body.errors);
+                expect(res.statusCode).toEqual(201);
+
+                return request.post('/api/users/register')
+                    .send(user)
+                    .then(res => {
+                        expect(res.statusCode).toEqual(422);
+                        console.log(res.body.errors);
+                        // expect(res.body._id).toEqual(user.email);
+
+                    })
+
+            })
+    })
 })
 
 
