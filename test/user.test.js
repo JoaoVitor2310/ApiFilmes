dee52d794c390e54e00fab4710af08dc372b384e
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-03-09 14:51:56 -0300

    afterAll utilizado, tirei o beforeAll pra não ficar redundante com o teste de criar

--- test/user.test.js
+++ test/user.test.js
@@ -2,25 +2,13 @@ const app = require('../src/app');
 const supertest = require('supertest');
 const request = supertest(app);
 
-const userForTests = { name: 'João Vitor', email: 'joaovitormatosgou@gmail.com', password: '1234567', confirmPassword: '1234567' }
 let user = { name: 'João Vitor', email: 'joaovitormatosgouveia@gmail.com', password: '1234567', confirmPassword: '1234567' };
 
-// beforeAll(() => {
-    //Registra um novo usuário para os testes
-    // return request.post('/api/users/register')
-    //     .send(userForTests)
-    //     .then(res => { });
-// })
-
-
 afterAll(() => {
-    //Deleta o usuário criado pros testes, e depois o usuário do teste de registro.
-    return request.delete(`/api/deleteuser/${userForTests.email}`)
-                .then(res => {
-                    return request.delete(`/api/deleteuser/${user.email}`)
-                                .then(res => {});
-                });
-})
+    //Deleta o usuário criado pros testes de registro.
+    return request.delete(`/api/users/deleteuser/${user.email}`)
+                .then(res => {});
+    })
 
 describe('Cadastro de usuário', () => {
     test('Deve cadastrar um usuário novo com sucesso', () => {
@@ -38,28 +26,14 @@ describe('Cadastro de usuário', () => {
             })
     })
 
-    // test('Deve impedir que o usuário cadastre um email repetido.', () => {
-    //     // let user = { name: 'João Vitor', email: 'joaovitormatosgouveia@gmail.com', password: '1234567', confirmPassword: '1234567' };
-
-    //     return request.post('/api/users/register')
-    //         .send(user)
-    //         .then(res => {
-    //             // console.log(res.body.errors);
-    //             expect(res.statusCode).toEqual(201);
-
-    //             return request.post('/api/users/register')
-    //                 .send(user)
-    //                 .then(res => {
-    //                     expect(res.statusCode).toEqual(422);
-    //                     console.log(res.body.errors);
-    //                     // expect(res.body._id).toEqual(user.email);
-
-    //                 })
-
-    //         })
-    // })
+    test('Deve impedir que o usuário cadastre um email repetido.', () => {
+        
+        return request.post('/api/users/register')
+            .send(user)
+            .then(res => {
+                // console.log(res.body.errors);
+                expect(res.statusCode).toEqual(422);
+                // expect(res.body._id).toEqual(user.email);
+            })
+    })
 })
-
-
-
-//joaovitormatosgouveia@gmail.com
